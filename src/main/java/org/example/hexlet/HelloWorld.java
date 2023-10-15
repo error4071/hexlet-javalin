5815b7519c98eb96d48bb7056541beb6787e2da2
error4071 <sergey_tsoy87@mail.ru>
2023-10-15 23:16:21 +0500

    update

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -4,12 +4,16 @@ import io.javalin.Javalin;
 
 public class HelloWorld {
     public static void main(String[] args) {
-
         var app = Javalin.create(config -> {
             config.plugins.enableDevLogging();
         });
 
-        app.get("/", ctx -> ctx.result("Hello World"));
+
+        app.get("/users/{id}/post/{postId}", ctx -> {
+            ctx.result("users" + ctx.pathParam("id"));
+            ctx.result("post" + ctx.pathParam("postId"));
+        });
+
         app.start(7070);
     }
 }
